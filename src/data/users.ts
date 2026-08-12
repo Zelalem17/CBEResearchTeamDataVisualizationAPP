908622a30233f8be5312c53cf245fb798d5486a4
Sew101 <46305450+Zelalem17@users.noreply.github.com>
2026-08-12 09:52:05 -0700

    Update users.ts

--- src/data/users.ts
+++ src/data/users.ts
@@ -31,13 +31,14 @@ export interface UserAccount {
   role: "admin" | "editor" | "viewer";
   displayName?: string;
 }
- export const users: UserAccount[] = [
-   { 
-     username: "zola",
+
+export const users: UserAccount[] = [
+  { 
+    username: "zola",
     passwordHash: "67a20ef9ca16dac572541eaa76e742683bf3785db5e52c5ae9a11d29e794e088", 
     role: "admin",
-     displayName: "Zelalem Belay" },
-export const users: UserAccount[] = [
+    displayName: "Zelalem Belay" 
+  },
   {
     username: "bule",
     passwordHash: "d88385afb362d3b8d4fc3783190fe3fe34c32e73516731e428e91b6966c9808c",
