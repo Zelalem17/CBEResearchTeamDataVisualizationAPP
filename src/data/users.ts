54ace31fb0b62980d2d341aa14c6bb678379c483
Sew101 <46305450+Zelalem17@users.noreply.github.com>
2026-08-12 12:58:49 -0700

    Update users.ts

--- src/data/users.ts
+++ src/data/users.ts
@@ -15,7 +15,11 @@ export interface UserAccount {
     passwordHash: "d88385afb362d3b8d4fc3783190fe3fe34c32e73516731e428e91b6966c9808c",
     role: "editor",
     displayName: "zola"
-  }
+  },
+     { username: "070695",
+   passwordHash: "887315f08fe20c8dd988313aaae4ff87d94d18ea7488d3c4f827b617c48684ad", 
+   role: "editor", 
+   displayName: "Zelalem Belay" },
 ]; 
 
 // Alias export to satisfy files importing USERS in uppercase
